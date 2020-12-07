0a630663e53daab7912901ba9e8cda05fe88aa55
Kevin-Garrison <kayjaygar150n@gmail.com>
2020-12-07 13:27:46 -0600

    Added binding in MainActivity to inflate view and code comments to all layouts

--- app/src/main/java/com/example/udacity_project_one/MainActivity.kt
+++ app/src/main/java/com/example/udacity_project_one/MainActivity.kt
@@ -2,18 +2,24 @@ package com.example.udacity_project_one
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.example.udacity_project_one.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    // Get the nav controller
+    // Get binding the nav controller
+    private lateinit var binding : ActivityMainBinding
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // Added binding with DataBindingUtil to inflate view
+        binding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_main)
         // Assign the nav controller
         navController = findNavController(R.id.myNavHostFragment)
         // Set up with an Action Bar - AppBarConfiguration
